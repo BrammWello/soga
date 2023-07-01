2f80259013b0447070db839ab59d4c4b159fd5a2
dev_bramm <bramwellokoth@gmail.com>
2023-07-01 11:56:09 +0300

    Onboarding Done

---
+++ app/src/main/java/com/devbramm/soga/activities/TermsConditionsActivity.java
@@ -0,0 +1,16 @@
+package com.devbramm.soga.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.devbramm.soga.R;
+
+public class TermsConditionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_conditions);
+    }
+}
---
+++ app/src/main/java/com/devbramm/soga/activities/adapters/OnboardingAdapter.java
@@ -0,0 +1,76 @@
+package com.devbramm.soga.activities.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.devbramm.soga.R;
+
+public class OnboardingAdapter extends PagerAdapter {
+
+    private Context context;
+    private LayoutInflater layoutInflater;
+
+    public OnboardingAdapter(Context context) {
+        this.context = context;
+        layoutInflater = LayoutInflater.from(context);
+    }
+
+    private int titles[] = {
+            R.string.onboarding_title_1,
+            R.string.onboarding_title_2,
+            R.string.onboarding_title_3
+    };
+
+    private int descriptions[] = {
+            R.string.onboarding_desc_1,
+            R.string.onboarding_desc_2,
+            R.string.onboarding_desc_3
+    };
+
+    private int images[] = {
+            R.drawable.onboarding_1,
+            R.drawable.onboarding_1,
+            R.drawable.onboarding_1
+    };
+
+    @Override
+    public int getCount() {
+        return titles.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (ConstraintLayout) object;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((ConstraintLayout) object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        View v = layoutInflater.inflate(R.layout.page_onboarding, container, false);
+
+        ImageView image = v.findViewById(R.id.iv_onboarding);
+        TextView title, description;
+        title = v.findViewById(R.id.tv_title_onboarding);
+        description = v.findViewById(R.id.tv_description_onboarding);
+
+        image.setImageResource(images[position]);
+        title.setText(titles[position]);
+        description.setText(descriptions[position]);
+
+        container.addView(v);
+        return v;
+    }
+}
